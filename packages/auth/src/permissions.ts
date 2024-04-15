f2421b6fda2d55bc784714cf650626ba633f5675
Rafael Souza <rafael.souza@iexfy.com>
2024-04-15 10:28:46 -0300

    definindo permissões do app

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -10,13 +10,18 @@ type PermissionsByRole = (
 ) => void
 
 export const permissions: Record<Role, PermissionsByRole> = {
-  ADMIN(_, { can }) {
+  ADMIN(user, { can, cannot }) {
     can('manage', 'all')
+
+    cannot('transfer_ownership', 'Organization')
+    can('transfer_ownership', 'Organization', { ownerId: { $eq: user.id } })
   },
   MEMBER(user, { can }) {
-    // can('invite', 'User')
+    can('get', 'User')
     can(['create', 'get'], 'Project')
     can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } })
   },
-  BILLING() {},
+  BILLING(_, { can }) {
+    can('manage', 'Billing')
+  },
 }
